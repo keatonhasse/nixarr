dd5bbe417259d541f273941b0f8c76a4cbb4d787
Keaton Hasse <git@keatonhasse.com>
2025-07-30 09:13:19 -0500

    add secrets replacement like upstream recyclarr module

--- nixarr/recyclarr/default.nix
+++ nixarr/recyclarr/default.nix
@@ -3,6 +3,7 @@
   lib,
   pkgs,
   inputs,
+  utils,
   ...
 }:
 with lib; let
@@ -213,6 +214,7 @@ in {
     systemd.services.recyclarr = {
       requires = ["recyclarr-setup.service"];
       after = ["recyclarr-setup.service"];
+      preStart = utils.genJqSecretsReplacementSnippet cfg.configuration configPath;
       serviceConfig = {
         ExecStart = lib.mkForce "${cfg.package}/bin/recyclarr sync --app-data ${cfg.stateDir} --config ${effectiveConfigFile}";
         EnvironmentFile = "${cfg.stateDir}/env";
